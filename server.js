df05a4904ed603debd970b24dac2f06ae352f767
Lee Hour <lhour999@gmail.com>
2023-02-08 20:52:05 +0700

    allow multiple origins

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+let allow = ["http://locgialhost:3000", "future deployed frontend"];
 
-const options = {
-  origin: "http://localhost:3000",
-  sucessStatus: 200,
-};
+function options(req, res) {
+  let temp;
+  let origin = req.header("origin");
+  if (allow.indexOf(origin) > -1) {
+    temp = { origin: true, succesStatus: 200 };
+    return res(null, tmp);
+  }
+  temp = {
+    origin: "nope",
+  };
+  return res(null, tmp);
+}
+// const options = {
+//   origin: "http://locgialhost:3000",
+//   sucessStatus: 200,
+// };
 
 app.use(cors(options));
 
