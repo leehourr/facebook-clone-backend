18a7591a1aac2791de30442d87b0bc1db1879d2a
Lee Hour <lhour999@gmail.com>
2023-02-09 13:19:12 +0700

    implemented Oauth and account verification

--- controller/user.js
+++ controller/user.js
@@ -66,8 +66,24 @@ exports.register = async (req, res) => {
       { id: user.id.toString() },
       "1d"
     );
-    console.log(emailVerificationToken);
-    res.json(user);
+    //   console.log(emailVerificationToken);
+    //   res.json(user);
+    // } catch (error) {
+    //   res.status(500).json({ message: error.message });
+    // }
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    sendVerificationEmail(user.email, user.first_name, url);
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Register Success ! please activate your email to start",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
