0c3afd02e04dbc9b7c39c0d83cc838f24eb8d0fd
Lee Hour <lhour999@gmail.com>
2023-02-09 13:28:47 +0700

    implemenetd acc activation

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,7 @@
 const { validateEmail, validateLength } = require("../helpers/validation");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { generateToken } = require("../helpers/jwtToken");
 
 exports.register = async (req, res) => {
@@ -87,4 +88,17 @@ exports.register = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+exports.activateAccount = async (req, res) => {
+  const { token } = req.body;
+  const user = jwt.verify(token, process.env.TOKEN_SECRET);
+  const check = await User.findById(user.id);
+  if (check.verified === true) {
+    return res.status(400).json({ message: "This email is already activated" });
+  }
+  await User.findByIdAndUpdate(user.id, { verified: true });
+  return res
+    .status(200)
+    .json({ message: "Account has been activated successfully." });
 };
