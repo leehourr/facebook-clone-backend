f39478daadac492df609dc8d29230199144a5b77
Lee Hour <lhour999@gmail.com>
2023-03-04 14:20:43 +0700

    added upload controller

---
+++ controller/upload.js
@@ -0,0 +1,8 @@
+exports.uploadImages = async (req, res) => {
+    try {
+      res.json("welcome from image upload");
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  };
+  
